1791ff4a27c37e4282bfaba027b171a85f8c99d3
notpsnk <frazzle.kibbles_0i@icloud.com>
2025-05-06 11:53:05 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,64 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+template <typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T data;
+    Node* link;
+
+    Node(const T& val) {
+      data = val;
+      link = nullptr;
+    }
+  };
+
+  Node* first = nullptr;
+
+ public:
+  TPQueue() {}
+
+  ~TPQueue() {
+    Node* walker = first;
+    while (walker != nullptr) {
+      Node* garbage = walker;
+      walker = walker->link;
+      delete garbage;
+    }
+  }
+
+  void push(const T& item) {
+    Node* unit = new Node(item);
+
+    if (!first || item.prior > first->data.prior) {
+      unit->link = first;
+      return;
+    }
+
+    Node* scout = first;
+    while (scout->link && scout->link->data.prior >= item.prior) {
+      scout = scout->link;
+    }
+
+    unit->link = scout->link;
+    scout->link = unit;
+  }
+
+  T pop() {
+    if (first == nullptr)
+      throw std::underflow_error("Queue is empty");
+
+    Node* temp = first;
+    T result = temp->data;
+    first = first->link;
+    delete temp;
+    return result;
+  }
+
+  bool isEmpty() const {
+    return first == nullptr;
+  }
 };
 
 struct SYM {
